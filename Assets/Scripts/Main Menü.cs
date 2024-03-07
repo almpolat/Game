ae5396c13934dbc90ef12ec07dc9f966f4a289af
almpolat <155629947+almpolat@users.noreply.github.com>
2024-03-07 16:12:22 +0300

    Initial commit

---
+++ Assets/Scripts/Main Menü.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
